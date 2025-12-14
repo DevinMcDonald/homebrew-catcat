84940f2cd8e7658192728d55fd8fe2f3673302e8
DevinMcDonald <devin1217@gmail.com>
2025-12-13 18:11:47 -0600

    catcat 1.3.2

--- Formula/catcat.rb
+++ Formula/catcat.rb
@@ -1,8 +1,8 @@
 class Catcat < Formula
   desc "Terminal tower defense with cats"
   homepage "https://github.com/DevinMcDonald/catcat"
-  url "https://github.com/DevinMcDonald/catcat/releases/download/v1.3.1/catcat_bundle.zip"
-  sha256 "e3c7699094c3e467fd4683a05fad26d1aedcc95125032bff8d4dae22a5795c36"
+  url "https://github.com/DevinMcDonald/catcat/releases/download/v1.3.2/catcat_bundle.zip"
+  sha256 "83c417b6df853309457f09bbec188d8e819187c5a6ff34f81965b1ce2840f723"
   license "MIT" # Update if your project uses a different license
 
   def install
